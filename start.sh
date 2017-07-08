fbee29316b39fc81f86fef7a9932917caffdd383
Gustavo Canedo <canedo@webgoal.com.br>
2017-07-08 07:46:21 -0300

    ajusta script de inicialização

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 docker-machine start || true
+docker-machine regenerate-certs || true
 eval $(docker-machine env) || true
 mv ~/Projects ~/.Projects || true
 mv ~/projects ~/.projects || true
 docker-compose build
 docker-compose pull
+docker-compose run app bundle install
 docker-compose up app
